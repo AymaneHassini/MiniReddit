91b468e42c659f2fc3e247075e271ad6a03ef9e1
AymaneHassini <aymane.hassini@gmail.com>
2023-10-13 22:12:20 +0100

    initial check-in

---
+++ MiniRedditt/src/main/java/com/MiniRedditt/MiniReddittApplication.java
@@ -0,0 +1,13 @@
+package com.MiniRedditt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniReddittApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniReddittApplication.class, args);
+	}
+
+}
