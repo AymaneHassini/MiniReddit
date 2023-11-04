d365e18011b868eb50111cd57d914331841957d5
AymaneHassini <aymane.hassini@gmail.com>
2023-11-04 14:20:59 +0100

    Creating 1st controller

---
+++ MiniRedditt/src/main/java/com/MiniRedditt/RootController.java
@@ -0,0 +1,12 @@
+package com.MiniRedditt;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+	@GetMapping("/")
+	public String rootView() {
+		return "index";
+	}
+}
