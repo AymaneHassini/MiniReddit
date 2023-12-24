1cfd03462f0d7f92d97c2e2e58b8634e0304f999
AymaneHassini <aymane.hassini@gmail.com>
2023-12-24 16:42:05 +0100

    Login Page

--- MiniRedditt/src/main/java/com/MiniRedditt/web/LoginController.java
+++ MiniRedditt/src/main/java/com/MiniRedditt/web/LoginController.java
@@ -1,12 +1,12 @@
-package com.MiniRedditt;
+package com.MiniRedditt.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class RootController {
-	@GetMapping("/")
-	public String rootView() {
-		return "index";
-	}
+public class LoginController {
+@GetMapping("/login")
+public String login() {
+return "login";
+}
 }
