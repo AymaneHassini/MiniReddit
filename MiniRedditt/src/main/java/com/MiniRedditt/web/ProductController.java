216013e8e0eb6e471e289d549ae9ba2bfbb79b81
AymaneHassini <aymane.hassini@gmail.com>
2024-01-25 01:45:00 +0100

    SavingProductToDB

--- MiniRedditt/src/main/java/com/MiniRedditt/web/ProductController.java
+++ MiniRedditt/src/main/java/com/MiniRedditt/web/ProductController.java
@@ -1,18 +1,39 @@
 package com.MiniRedditt.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.MiniRedditt.domain.Product;
+import com.MiniRedditt.domain.User;
+import com.MiniRedditt.repositories.ProductRepository;
 
 @Controller
 public class ProductController {
+	@Autowired
+	private ProductRepository productRepo;
+
 	@GetMapping("/products")
 	public String getProducts(ModelMap model) {
 		return "product";
 	}
+
+	@GetMapping("/products/{productId}")
+	public String getProduct(@PathVariable Long productId) {
+		return "product";
+	}
+
+	// HTTP post means create
 	@PostMapping("/products")
-	public String createProduct() {
-		return "redirect:/products"; // always redirect when doing a post
+	public String createProduct(@AuthenticationPrincipal User user) {
+		// annotation uses the authen user
+		Product product = new Product();
+		product.setPublished(false);
+		product = productRepo.save(product);
+		return "redirect:/products/" + product.getId(); // always redirect when doing a post
 	}
 }
