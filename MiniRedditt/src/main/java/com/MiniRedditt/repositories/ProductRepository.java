521820e37287d4040d01a1a7f0508882130d5c78
AymaneHassini <aymane.hassini@gmail.com>
2024-01-25 01:45:29 +0100

    Revert "SavingProductToDB"
    This reverts commit 216013e8e0eb6e471e289d549ae9ba2bfbb79b81.

--- MiniRedditt/src/main/java/com/MiniRedditt/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.MiniRedditt.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.MiniRedditt.domain.Product;
-//crud ops
-public interface ProductRepository extends JpaRepository<Product,Long> {
-
-}
